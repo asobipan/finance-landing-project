f6c97d63a42f5acb9c7309913f0705fee08a72da
Arsen Sobipan <sobipanarsen@gmail.com>
2024-11-06 01:56:51 +0200

    Add main features, more features and preview sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,2 @@
 import "./modules/banner";
+import "./modules/preview";
---
+++ src/js/modules/preview.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".preview__slider").slick({
+    infinite: true,
+    speed: 500,
+    cssEase: "linear",
+    arrows: false,
+    dots: true,
+    fade: true,
+  });
+});
