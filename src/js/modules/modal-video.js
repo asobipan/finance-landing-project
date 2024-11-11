e095e6158a6299d1e1486d60f9788684da4461b1
Arsen Sobipan <sobipanarsen@gmail.com>
2024-11-11 23:53:29 +0200

    Add overflow hidden feature to video modal

--- src/js/modules/modal-video.js
+++ src/js/modules/modal-video.js
@@ -6,12 +6,14 @@ const videoURL = "https://www.youtube.com/embed/iR7b2NjgAO8?mute=1";
 
 videoOverlay.addEventListener("click", () => {
   videoModal.classList.add("open__video");
+  document.body.style.overflow = "hidden";
   video.src = videoURL + "&autoplay=1";
 });
 
 const closeVideoModal = () => {
   videoModal.classList.remove("open__video");
   video.src = "";
+  document.body.style.overflow = "initial";
 };
 
 videoModal.addEventListener("click", closeVideoModal);
